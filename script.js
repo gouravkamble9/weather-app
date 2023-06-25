7f62bda57d5aafe8199037547a2c7b720eda8df9
gouravkamble9 <61933116+gouravkamble9@users.noreply.github.com>
2023-06-25 12:52:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,19 +50,19 @@ async function checkWeather(city,lat,long){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/assets/cloud.png";
+            weather_img.src = "assets/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/assets/clear.png";
+            weather_img.src = "assets/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/assets/rainy.png";
+            weather_img.src = "assets/rainy.png";
             break;
         case 'Mist':
-            weather_img.src = "/assets/mist.png";
+            weather_img.src = "assets/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/assets/snow.png";
+            weather_img.src = "assets/snow.png";
             break;
 
     }
