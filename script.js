ab7f7ff1ecec3d7b168caa34ad7da194768a9591
gouravkamble9 <61933116+gouravkamble9@users.noreply.github.com>
2023-06-25 13:24:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,19 +50,19 @@ async function checkWeather(city,lat,long){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png";
+            weather_img.src = "/assets/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png";
+            weather_img.src = "/assets/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png";
+            weather_img.src = "/assets/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png";
+            weather_img.src = "/assets/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png";
+            weather_img.src = "/assets/snow.png";
             break;
 
     }
